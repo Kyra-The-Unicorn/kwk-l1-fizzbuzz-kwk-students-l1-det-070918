ebfea772d9b555594511ce22fc2d7d24f4b0a222
Kyra-The-Unicorn <kyrastinson90@gmail.com>
2018-07-11 15:26:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz()
-  if 
-  
+def fizzbuzz(num)
+  if num % 3 == 0
+    puts "Fizz"
+    elsif num % 5 == 0
+    puts "Buzz"
+    elsif num % 3 && 5 == 0
+    puts "FizzBuzz"
+  else
+    puts "nil"
+  end
+end
